e612903cf76cc15d9deefeb2649466b2e3ea8355
zhangyajie <z_yajie@sina.com>
2019-01-21 10:38:19 +0800

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -34,4 +34,10 @@ Route::resource('users', 'UsersController');
 //Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
 
 
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
+
+
 
